5e2213b3a7f24cdde07d802e1e68c45f6914315a
Felipe Andrade <felipe.andrade0918@gmail.com>
2018-08-29 21:21:08 -0300

    Rename class

--- src/main/java/com/batutapps/facebooklogin/FacebookLoginApplication.java
+++ src/main/java/com/batutapps/facebooklogin/FacebookLoginApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FacebookpagesApplication {
+public class FacebookLoginApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FacebookpagesApplication.class, args);
+		SpringApplication.run(FacebookLoginApplication.class, args);
 	}
 }
